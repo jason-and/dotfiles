af0cb8e2e5b40a787be6b5871a53dc0371d905c2
Andrew Jason <andrew.r.jason@gmail.com>
2024-11-20 15:39:53 -0600

    added wez terminal config

---
+++ .wezterm.lua
@@ -0,0 +1,57 @@
+-- pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- this will hold the configuration
+local config = wezterm.config_builder()
+
+-- config
+
+--- Appearance
+--config.color_scheme = 'Belafonte Night (Gogh)'
+config.color_scheme = 'Tomorrow (dark) (terminal.sexy)'
+--config.color_scheme = 'Twilight (Gogh)'
+
+
+---- Fonts and window
+
+config.window_background_opacity = 1.0
+
+config.font_size = 14
+config.line_height = 1
+
+config.harfbuzz_features = { 'zero' }
+
+config.dpi = 96.0
+config_bold_brightens_ansi_colors = true
+
+config.window_padding = {
+left = "32pt", 
+right = "32pt",
+bottom = "32pt",
+top = "32pt",
+        }
+
+
+-- SIZE of window:
+config.initial_rows = 55
+config.initial_cols = 130
+
+config.window_decorations = "RESIZE"
+
+---Cursor
+config.default_cursor_style = 'BlinkingBlock'
+config.cursor_blink_rate = 600
+
+
+--- Tab Bar
+enable_tab_bar = true
+use_fancy_tab_bar = false
+hide_tab_bar_if_only_one_tab = true
+show_tab_index_in_tab_bar = false
+
+
+window_close_confirmation = "NeverPrompt"
+
+
+-- and finally, return the configuration to wezterm
+return config
